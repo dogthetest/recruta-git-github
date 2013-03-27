c7eed590b8ad2f47f87556ce7229a229bd7d11c3
dogthetest <dog3k3000-cadastros@yahoo.com.br>
2013-03-27 20:45:06 -0300

    novo metodo

--- index.php
+++ index.php
@@ -6,5 +6,10 @@ $app = new Silex\Application();
 $app->get('/hello/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name);
 });
+
+$app->get('/', function() use($app) {
+    return 'OK';
+});
+
 //executa
 $app->run();
